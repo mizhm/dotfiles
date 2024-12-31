d55f788a4bb4260292fee30d74016724d5625e4d
mizhm <minhnt141204@gmail.com>
2024-12-31 21:50:38 +0700

    first commit

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+starship init fish | source
+alias c="clear"
+alias pn="pnpm"
+alias v="nvim"
+alias nv="nvim"
+export PATH="$PATH:/opt/nvim-linux64/bin"
+export PATH="$HOME/opt/usr/local/bin:$PATH"
+export TMPDIR="/tmp"
+
